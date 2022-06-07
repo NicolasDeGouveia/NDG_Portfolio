8a4c7573717c034be9825d70d6dceededae6fce9
unknown <nicolas.degouveia@gmail.com>
2022-06-07 15:30:56 +0200

    fixing image on mobile navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
               <Link href="/">
                 <Image
                   className="cursor-pointer"
-                  src="/../public/assets/logo.png "
+                  src={logo}
                   alt="/"
                   width="125"
                   height="100"
